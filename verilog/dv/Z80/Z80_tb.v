64af5418a32563ff44234b39f866d3a05a8c72ae
rej <rzioma@acm.org>
2024-06-05 13:42:25 +0200

    Remapped pins between Z80 and Caravel to reduce wires crossing on PCB

--- verilog/dv/Z80/Z80_tb.v
+++ verilog/dv/Z80/Z80_tb.v
@@ -30,25 +30,66 @@ module Z80_tb;
 	reg design_rst;
 	assign mprj_io[0] = design_rst;
 	
-	wire halt_n = mprj_io[1];
-	wire busak_n = mprj_io[2];
-	wire m1_n = mprj_io[4];
-	wire mreq_n = mprj_io[5];
-	wire iorq_n = mprj_io[6];
-	wire rd_n = mprj_io[7];
-	wire wr_n = mprj_io[8];
-	wire rfsh_n = mprj_io[9];
-	wire [15:0] address = mprj_io[25:10];
-	wire [7:0] dbus_out = mprj_io[33:26];
+    // 5th revision:
+    // c[]  - caravel mprj_io[] pin
+    // io[] - multiplexer's io_in/out/oebp[] port
+    //                   ,----------------.___.----------------.
+    //      <--    A11 1 |io[17] c[19] 57       55 c[18] io[16]|40 A10    -->
+    //      <--    A12 2 |io[18] c[20] 58       54 c[17] io[15]|39 A9     -->
+    //      <--    A13 3 |io[29] c[21] 59       53 c[16] io[14]|38 A8     -->
+    //      <--    A14 4 |io[20] c[22] 60       51 c[15] io[13]|37 A7     -->
+    //      <--    A15 5 |io[21] c[23] 61       50 c[14] io[12]|36 A6     -->
+    //      -->    CLK 6 |------ ocs_o 22       48 c[13] io[11]|35 A5     -->
+    //      <->     D4 7 |io[22] c[24] 62       42 c[ 8] io[ 6]|34 A4     -->
+    //      <->     D3 8 |io[23] c[25]  2       43 c[ 9] io[ 7]|33 A3     -->
+    //      <->     D5 9 |io[24] c[26]  3       44 c[10] io[ 8]|32 A2     -->
+    //      <->     D6 10|io[25] c[27]  4       46 c[12] io[10]|31 A1     -->
+    //         VCC_5V0 11|                      45 c[11] io[ 9]|30 A0     -->
+    //      <->     D2 12|io[26] c[28]  5                      |29 GND
+    //      <->     D7 13|io[27] c[29]  6       41 c[ 7] io[ 5]|28 /RFSH  -->
+    //      <->     D0 14|io[29] c[31]  8       33 c[ 2] io[*1]|27 /M1    -->
+    //      <->     D1 15|io[28] c[30]  7       31 c[ 0] ------|26 /RESET <--
+    //      -->   /INT 16|io[31] c[33] 12       16 c[37] io[35]|25 /BUSRQ <--
+    //      -->   /NMI 17|io[30] c[32] 11       37 c[ 6] io[ 4]|24 /WAIT  <--
+    //      <--  /HALT 18|io[32] c[34] 13 *     32 c[ 1] io[*0]|23 /BUSAK -->
+    //      <--  /MREQ 19|io[33] c[35] 14 *     36 c[ 5] io[ 3]|22 /WR    -->
+    //      <--  /IORQ 20|io[34] c[36] 15 *     35 c[ 4] io[ 2]|21 /RD    -->
+    //                   `-------------------------------------'
+
+	wire busak_n = mprj_io[1];
+	wire m1_n = mprj_io[2];
+	wire rd_n = mprj_io[4];
+	wire wr_n = mprj_io[5];
+	wire rfsh_n = mprj_io[7];
+	wire halt_n = mprj_io[34];
+	wire mreq_n = mprj_io[35];
+	wire iorq_n = mprj_io[36];
+	wire [15:0] address = {mprj_io[23:13], mprj_io[8], mprj_io[9], mprj_io[10], mprj_io[12], mprj_io[11]};
+	wire [7:0] dbus_out = {	mprj_io[29],  // D7
+							mprj_io[27],
+							mprj_io[26],
+							mprj_io[24],
+							mprj_io[25],
+							mprj_io[28],
+							mprj_io[30],
+							mprj_io[31]}; // D0
 	reg [7:0] dbus_in;
-	assign mprj_io[33:26] = rd_n ? 8'hzz : dbus_in;
+	assign mprj_io[29] = rd_n ? 1'bz : dbus_in[7];
+	assign mprj_io[27] = rd_n ? 1'bz : dbus_in[6];
+	assign mprj_io[26] = rd_n ? 1'bz : dbus_in[5];
+	assign mprj_io[24] = rd_n ? 1'bz : dbus_in[4];
+	assign mprj_io[25] = rd_n ? 1'bz : dbus_in[3];
+	assign mprj_io[28] = rd_n ? 1'bz : dbus_in[2];
+	assign mprj_io[30] = rd_n ? 1'bz : dbus_in[1];
+	assign mprj_io[31] = rd_n ? 1'bz : dbus_in[0];
+	
 	reg int_n;
 	reg nmi_n;
 	reg wait_n;
 	reg busrq_n;
-	assign mprj_io[34] = int_n;
-	assign mprj_io[35] = nmi_n;
-	assign mprj_io[36] = wait_n;
+	assign mprj_io[6] = wait_n;
+	assign mprj_io[32] = nmi_n;
+	assign mprj_io[33] = int_n;
 	assign mprj_io[37] = busrq_n;
 	
 	assign mprj_io[3] = CSB;
@@ -286,6 +327,7 @@ module Z80_tb;
 		
 		dbus_in = 0;
 		repeat(5) @(posedge clock);
+		$display(failures);
 		if(failures != 0) begin
 			$display(failures);
 			$display("%c[1;31m",27);
